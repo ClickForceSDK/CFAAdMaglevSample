7ee59bc570e2202b01e55f428e11a8376a8095d0
ClickForce <clickforce01@gmail.com>
2022-02-09 10:05:02 +0800

    Update Clickforce SDK 3.9.2

--- app/src/main/java/com/force/click/cfaadmaglevsample/MainActivity.java
+++ app/src/main/java/com/force/click/cfaadmaglevsample/MainActivity.java
@@ -1,8 +1,10 @@
 package com.force.click.cfaadmaglevsample;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.clickforce.ad.AdMaglev;
 import com.clickforce.ad.Listener.AdMaglevListener;
@@ -13,8 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        adMaglev = new AdMaglev(this);
-        adMaglev.init(this);
+        adMaglev = new AdMaglev(this, AdMaglev.TOP | AdMaglev.RIGHT);
         adMaglev.getAdMaglev(9800);
         adMaglev.outputDebugInfo = true;
         adMaglev.setOnAdMaglevListener(new AdMaglevListener() {
